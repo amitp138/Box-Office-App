e3d2eebf2f33c4767711ea6f30156fc09c33ee9a
amitpandey <amitpande1008@gmail.com>
2022-06-19 18:26:56 +0530

    added seach bar and fetch data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState} from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
+  const [input,setInput]=useState('')
+  const handleInput=(e)=>{
+    setInput(e.target.value)
+  }
+  const handleSearch=()=>{
+   
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+  }
+  const onkeydown=(event)=>{
+    if(event.keyCode===13){
+      handleSearch();
+    }
+  }
   return (
-    <MainPageLayout>Home</MainPageLayout>
+    <MainPageLayout>
+      <input type="text" value={input} onKeyDown={onkeydown} onChange={handleInput}/>
+      <button type='button'  onClick={handleSearch}>search</button>
+    </MainPageLayout>
   )
 }
 
