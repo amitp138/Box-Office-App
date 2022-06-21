977d17692736ef87ff7223b8afe45e755913bbf5
amitpandey <amitpande1008@gmail.com>
2022-06-21 15:44:56 +0530

    added styles and show,actor components

--- src/misc/config.js
+++ src/misc/config.js
@@ -1,6 +1,8 @@
- const API_BASE_URL="https://api.tvmaze.com"
-export async function apiGet(queryString){
-  const resultQuery=  fetch(`${API_BASE_URL}${queryString}`)
-    .then(response => response.json())
-    return resultQuery
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(r =>
+    r.json()
+  );
+  return response;
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,92 @@
-import React,{useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
-import {apiGet} from "../misc/config"
+import React, { useState } from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
+import ActorGrid from '../components/actor/ActorGrid';
+
 const Home = () => {
-  const [input,setInput]=useState('')
-  const [data,setData]=useState("")
-  const handleInput=(e)=>{
-    setInput(e.target.value)
-  }
-  const handleSearch=()=>{
-   apiGet(`/search/shows?q=${input}`).then(result => {
-      setData(result)
-      console.log(result)});
-   
-  }
-  const onkeydown=(event)=>{
-    if(event.keyCode===13){
-      handleSearch();
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
+
+  const isShowsSearch = searchOption === 'shows';
+
+  const onSearch = () => {
+    apiGet(`/search/${searchOption}?q=${input}`).then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
     }
-  }
-  const showResults=()=>{
-    if(data && data.length===0){
-      return <div>no results</div>
+  };
+
+  const onRadioChange = ev => {
+    setSearchOption(ev.target.value);
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
     }
-    if(data && data.length>0){
- return <div>{data.map((item)=>{
- return <div key={item.show.id}>{item.show.name}</div>
- })}</div>
+
+    if (results && results.length > 0) {
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
-    return null
-  }
+
+    return null;
+  };
+
   return (
     <MainPageLayout>
-      <input type="text" value={input} onKeyDown={onkeydown} onChange={handleInput}/>
-      <button type='button'  onClick={handleSearch}>search</button>
-      {showResults()}
-    </MainPageLayout>
-  )
-}
+      <input
+        type="text"
+        placeholder="Search for something"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
 
-export default Home
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            id="shows-search"
+            type="radio"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+
+        <label htmlFor="actors-search">
+          Actors
+          <input
+            id="actors-search"
+            type="radio"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
+
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+    </MainPageLayout>
+  );
+};
+
+export default Home;
