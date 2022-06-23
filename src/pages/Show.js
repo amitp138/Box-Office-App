a277cc198a882863f39e6971ad9fdbd51ec1200c
amitpandey <amitpande1008@gmail.com>
2022-06-23 11:20:09 +0530

    added show pages

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,10 @@
 import React,{useEffect,useReducer} from 'react'
 import { useParams } from 'react-router-dom'
+import ShowMainData from '../components/show/ShowMainData';
+import Details from '../components/show/Details';
 import {apiGet} from '../misc/config'
+import Seasons from '../components/show/Seasons';
+import Cast from '../components/show/Cast';
 const initialstate={
   isLoading:true,
   error:null,
@@ -47,9 +51,34 @@ const Show = () => {
     }
   return (
     <div>
-        this is show
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} />
+      </div>
+
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Show
